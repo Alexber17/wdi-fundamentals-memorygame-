fc7691ef06e15d9b1d5b8933105de3157f9c0a5e
Alexber17 <alexber17@gmail.com>
2020-01-26 23:00:48 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,61 @@
-console.log("Up and running!");
-let cardOne = "king";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "queen";
+//console.log("Up and running!");
 
-console.log("User Flipped " + cardTwo);
+
+
+//const cards=["queen","queen","king","king"];
+
+const cards = [
+{
+rank: "queen",
+suit	: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen", 
+suit	: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit	: "hearts",
+cardImage:"images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit	: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+
+];
+
+const cardsInPlay=[];
+
+function checkForMatch() {
+	 if (cardsInPlay[0]===cardsInPlay[1]) {
+
+ 	alert("You found a match!");
+
+ } else {alert("Sorry, try again.!");}
+	// body...
+}
+
+
+function flipCard(cardId) {
+	// body...
+console.log("User flipped " + cards[cardId].rank);
+console.log("IMG " + cards[cardId].cardImage);
+console.log("Suite " + cards[cardId].suit);
+console.log("------------------------------- ");
+
+
+cardsInPlay.push(cards[cardId].rank);
+ 
+if (cardsInPlay.length===2) {
+ //console.log("jugaron 2 castas")
+
+checkForMatch();
+}
+
+}
+flipCard(0);
+flipCard(1);
